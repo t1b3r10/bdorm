8cb440561bba82af4c7b9a73668c664bb818a48a
Tiberio Lima <tiberio.lima@gmail.com>
2023-07-01 20:12:23 -0300

    h2 config table tb_department

--- src/main/java/com/devsuperior/bdorm/entities/Department.java
+++ src/main/java/com/devsuperior/bdorm/entities/Department.java
@@ -2,8 +2,19 @@ package com.devsuperior.bdorm.entities;
 
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name="tb_department")
 public class Department {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	
