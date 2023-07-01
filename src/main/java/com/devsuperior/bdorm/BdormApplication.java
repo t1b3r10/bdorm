f035cbdbd96e6eddc7e935d3331d399c1aa9aca1
Tiberio Lima <tiberio.lima@gmail.com>
2023-06-30 21:47:01 -0300

    first commit

---
+++ src/main/java/com/devsuperior/bdorm/BdormApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.bdorm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BdormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BdormApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/bdorm/BdormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.bdorm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BdormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
